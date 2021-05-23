d282e1ad6a84ba65a6b01b038826aa3e9799e072
David Levai <david@davidlevai.com>
2021-05-23 15:50:07 +0200

    add fathom

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,13 @@ import { ThemeProvider } from 'next-themes'
 import { DefaultSeo } from 'next-seo'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-
+import { useAnalytics } from '@lib/analytics'
 import { SEO } from '@/components/SEO'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import MDXComponents from '@/components/MDXComponents'
 
 export default function App({ Component, pageProps }) {
+  useAnalytics()
   const { pathname } = useRouter()
   return (
     <ThemeProvider attribute="class">
