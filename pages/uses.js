fc0b8d5a17b603ac6b36012774ea31fc307e01f4
David Levai <david@davidlevai.com>
2021-05-23 16:16:38 +0200

    fix spacing on uses & social

--- pages/uses.js
+++ pages/uses.js
@@ -23,10 +23,14 @@ export default function Uses() {
           </p>
         </div>
         <div className="container py-12">
-          <div className="grid grid-cols-2">
+          <div className="flex flex-row flex-wrap">
             {usesData.map((d) => (
-              <Link key={d.href} href={d.href}>
-                <a className="flex items-center justify-start w-full col-span-1 p-6 hover:bg-gray-100 hover:bg-opacity-40 hover:rounded-xl group dark:hover:bg-opacity-40 dark:hover:bg-gray-500 backdrop-filter transition duration-200 cursor-pointer mb-8">
+              <Link
+                key={d.href}
+                href={d.href}
+                className="w-full md:w-1/2 p-6 hover:bg-gray-100 hover:bg-opacity-40 hover:rounded-xl group dark:hover:bg-opacity-40 dark:hover:bg-gray-500 backdrop-filter transition duration-200 cursor-pointer mb-4"
+              >
+                <a className="flex items-center justify-start">
                   <div className="flex-shrink-0 p-3 font-sans text-gray-700 dark:text-gray-50 ">
                     <svg
                       width={36}
