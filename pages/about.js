325d50fd3f22f98bf3dd71b7463f397e74cca6c2
David Levai <david@davidlevai.com>
2021-05-30 15:33:23 +0200

    add about content

--- pages/about.js
+++ pages/about.js
@@ -41,20 +41,60 @@ export default function About() {
             <p>
               I create various types of content across{' '}
               <Link href={siteMetadata.twitter}>Twitter</Link>,{' '}
-              <Link href={siteMetadata.youtube}>YouTube</Link>, and this blog about{' '}
+              <Link href={siteMetadata.youtube}>YouTube</Link>,{' '}
+              <Link href={siteMetadata.linkedin}>LinkedIn</Link>, and this blog about{' '}
               <strong>building IT businesses and production-ready apps.</strong>
             </p>
             <p>
-              I worked as a freelancer full-stack developer, spent my time working with startups and
-              small businesses since my teenage years and now I started translating this knowledge
-              into 2 things. <strong>Scaling a digital agency and building open startups.</strong>
+              Since my teenage years, I have worked as a freelance full-stack developer mostly with
+              startups and small businesses. In 2020 I decided to use this knowledge to scale a
+              digital agency and build open startups.
+            </p>
+            <h2>Why I'm doing these in public?</h2>
+            <ul>
+              <li>
+                I believe that learning from others by following their journey is inspiring and
+                helpful. I too follow tens of creators on their journey.
+              </li>
+              <li>
+                By being active in social media, and building in public, I can meet with interesting
+                people, and support each other.
+              </li>
+            </ul>
+            <h2>What you'll find here?</h2>
+            <p>
+              Vlogs, essays, tips, and discussions. About building an international digital product
+              agency, building production-ready apps, and being mindful, productive as an
+              entrepreneur in IT. Feel free to read about my background, knowledge, experience, and
+              my why in the next few paragraphs.
             </p>
             <p>
-              This page is currently a draft. Look back later to see what's changed.{' '}
-              <span role="img" aria-label="roadwork sign">
-                🚧
-              </span>
+              When I'm not creating content, or managing a business, I am a full-stack developer who
+              started as a UI designer. Enthusiastic about growth mindset and self-development,
+              passionate about digital marketing, and UX. Always on the lookout for how to maximize
+              productivity and my teams' efficiency.
             </p>
+            <p>
+              <strong>My background as a Software Engineer:</strong> I create user-centered web and
+              mobile applications using cutting-edge technologies like React Native, Flutter,
+              GraphQL, and ESNext for almost 10 years. I love to learn new things by experimenting
+              with state-of-the-art technologies (mostly JS-related).
+            </p>
+            <p>
+              <strong>My background as a Content Creator:</strong> I like sharing my knowledge with
+              others, so I write for several tech blogs and authoring my blog too. I also create
+              content on LinkedIn, Twitter, and Youtube to teach software devs how to be successful
+              as freelancers, and how to write truly production-ready apps.
+            </p>
+            <p>
+              <strong>My background as a Manager:</strong> I've got experience in managing small &
+              medium-sized product development teams by running a digital agency. We successfully
+              finished several projects that were started from the ground up with lean, agile
+              methods for multiple clients. I managed the projects from estimation & proposal,
+              through design & development, to marketing & maintenance. Later, I joined an
+              international digital product agency, ScreamingBox as a COO.
+            </p>
+            <p></p>
           </div>
         </div>
       </div>
