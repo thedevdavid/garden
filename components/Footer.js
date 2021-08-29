4d755c5a8ae486c3f56555da23b39baa93504bca
David Levai <david@davidlevai.com>
2021-08-29 17:22:34 +0200

    add links to footer for crawlers

--- components/Footer.js
+++ components/Footer.js
@@ -16,12 +16,68 @@ export default function Footer() {
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
           <SocialIcon kind="tiktok" href={siteMetadata.tiktok} size="6" />
         </div>
-        <div className="flex mb-8 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
+        </div>
+        <div className="flex mb-8 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <Link
+            className="hover:text-primary-600 dark:hover:text-primary-600 hover:underline"
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="hover:text-primary-600 dark:hover:text-primary-600 hover:underline"
+            href="/content"
+          >
+            Content
+          </Link>
+          <Link
+            className="hover:text-primary-600 dark:hover:text-primary-600 hover:underline"
+            href="/tags"
+          >
+            Tags
+          </Link>
+          <Link
+            className="hover:text-primary-600 dark:hover:text-primary-600 hover:underline"
+            href="/about"
+          >
+            About Me
+          </Link>
+          <Link
+            className="hover:text-primary-600 dark:hover:text-primary-600 hover:underline"
+            href="/projects"
+          >
+            Projects
+          </Link>
+          <Link
+            className="hover:text-primary-600 dark:hover:text-primary-600 hover:underline"
+            href="/mentoring"
+          >
+            Mentoring
+          </Link>
+          <Link
+            className="hover:text-primary-600 dark:hover:text-primary-600 hover:underline"
+            href="/saas-kit"
+          >
+            SaaS Kit
+          </Link>
+          <Link
+            className="hover:text-primary-600 dark:hover:text-primary-600 hover:underline"
+            href="/uses"
+          >
+            Uses
+          </Link>
+          <Link
+            className="hover:text-primary-600 dark:hover:text-primary-600 hover:underline"
+            href="/social"
+          >
+            Social
+          </Link>
         </div>
       </div>
     </footer>
