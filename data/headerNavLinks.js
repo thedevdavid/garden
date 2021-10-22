7b7662f00e2f8ea53177059b73a25e7fb25a0b10
David Levai <david@davidlevai.com>
2021-10-22 10:18:57 +0200

    added timeline link to polywork

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -4,6 +4,7 @@ const headerNavLinks = [
   { href: '/about', title: 'About' },
   // { href: '/saas-kit', title: 'SaaS Kit' },
   { href: '/uses', title: 'Uses' },
+  { href: 'https://timeline.davidlevai.com', title: 'Timeline' },
   // { href: '/tags', title: 'Tags' },
 ]
 
