e1aafc686776cd7eae191d1bab1cab2a08bf891c
Dávid Lévai <david@davidlevai.com>
2023-05-11 05:53:36 +0000

    feat: cleaned up uses

--- data/usesData.js
+++ data/usesData.js
@@ -17,7 +17,7 @@ const usesData = [
   {
     title: 'LunchMoney *',
     description: `Budgeting and money management`,
-    href: 'https://lunchmoney.app/?refer=3lxx87ps',
+    href: 'https://lunchmoney.app?fp_ref=david10',
   },
   {
     title: 'Raycast',
@@ -30,49 +30,29 @@ const usesData = [
     href: 'https://superhuman.com',
   },
   {
-    title: 'RescueTime (*)',
+    title: 'Rize',
     description: `Tracking and analyzing my time`,
-    href: 'https://www.rescuetime.com/ref/1111282',
-  },
-  {
-    title: 'Reclaim *',
-    description: `Blocking my time and managing my availability for my coworkers`,
-    href: 'https://reclaim.ai/r/s/g6YY2',
+    href: 'https://rize.io/',
   },
   {
     title: 'VSCode',
     description: `Click here to find my plugins & config`,
-    href: 'https://gist.github.com/DLevai94/c1b69f72a7deb472506ee3cf5f559f81',
+    href: 'https://gist.github.com/thedevdavid/c1b69f72a7deb472506ee3cf5f559f81',
   },
   {
-    title: 'Hyper & Oh My Zsh',
+    title: 'iTerm2 & Oh My Zsh',
     description: `Click here to find my plugins & config`,
-    href: 'https://gist.github.com/DLevai94/af307fb45d525ffebf38750970f98ed2',
+    href: 'https://gist.github.com/thedevdavid/af307fb45d525ffebf38750970f98ed2',
   },
   {
-    title: 'Daily.dev *',
-    description: `Homepage to stay up to date in the dev world`,
-    href: 'https://api.daily.dev/get?r=DavidLevai',
-  },
-  {
-    title: 'EmailOctopus *',
+    title: 'ActiveCampaign',
     description: `Newsletter, automations, and being in touch with my subscribers.`,
-    href: 'https://emailoctopus.com/?urli=tPw76',
+    href: 'activecampaign.com',
   },
   {
-    title: 'LumaFusion',
-    description: `I edit all my videos on iPad Pro with LumaFusion. It's 🔥`,
-    href: 'https://apps.apple.com/us/app/lumafusion/id1062022008',
-  },
-  {
-    title: 'Canva (*)',
-    description: `Sometimes I use Canva to edit some images quickly.`,
-    href: 'https://www.canva.com/join/fjj-qgr-dgh',
-  },
-  {
-    title: 'Obsidian',
-    description: `Secure, locally stored research and notes. Native iOS and Mac apps. Syncs well through iCloud`,
-    href: 'https://obsidian.md/',
+    title: 'Substack',
+    description: `Main newsletter`,
+    href: 'https://substack.com',
   },
   {
     title: 'Doppler (*)',
@@ -85,25 +65,11 @@ const usesData = [
     href: 'https://figma.com',
   },
   {
-    title: 'Notion *',
+    title: 'Notion (*)',
     description: `Permanent docs, organizing my life and my comapny`,
     href: 'https://www.notion.so/?r=b9b039621fdc4230b444f84cb877508c',
   },
   {
-    title: 'Timestripe',
-    description: `I started using this at the end of 2020. Swithed from Todoist. Highly recommend.`,
-    href: 'https://timestripe.com/',
-  },
-  {
-    title: 'Bitwarden',
-    description: `I started using this at the end of 2020. Swithed from Todoist. Highly recommend.`,
-    href: 'https://bitwarden.com/',
-  },
-  {
-    title: 'Coinbase (*)',
-    description: `Playing with crypto.`,
-    href: 'https://www.coinbase.com/join/dvid_wvr',
-  },
   {
     title: 'Cron',
     description: `The calendar app of my dreams. No questions.`,
